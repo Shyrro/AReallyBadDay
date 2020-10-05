b64df2fe13dc728c91c1f3f5e97271a2b4d4e9d5
Shyrro <zsahmane@gmail.com>
2020-10-05 20:58:55 +0200

    Translated story file to english + add tutorial board

--- Assets/Scripts/ButtonManager.cs
+++ Assets/Scripts/ButtonManager.cs
@@ -4,6 +4,7 @@ using UnityEngine.SceneManagement;
 public class ButtonManager : MonoBehaviour {
     
     private AudioManager audioManager;
+    public GameObject tutorialPanel;
 
     private void Awake() {
         audioManager = FindObjectOfType<AudioManager>();
@@ -17,6 +18,16 @@ public class ButtonManager : MonoBehaviour {
 
     public void QuitGame() {
         Application.Quit();
+    }
+
+    public void TutorialButton() {
+        audioManager.Play("buttonClick");        
+        tutorialPanel.SetActive(true);
+    }
+
+    public void CloseTutorialPanel(){
+        audioManager.Play("buttonClick");        
+        tutorialPanel.SetActive(false);
     }
 
 }
